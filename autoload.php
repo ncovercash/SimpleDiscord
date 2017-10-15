64eb453dd9c68464c51c6dfbe9d5246ad2370220
Noah Overcash <smileytechguy@icloud.com>
2017-10-15 10:21:13 -0400

    Image substructure autoload

--- autoload.php
+++ autoload.php
@@ -12,3 +12,4 @@ require_once "src/RestClient/Resources/BaseResource.php";
 require_once "src/RestClient/Resources/Gateway.php";
 require_once "src/Structures/Structure.php";
 require_once "src/Structures/Substructures/Substructure.php";
+require_once "src/Structures/Substructures/Image.php";
