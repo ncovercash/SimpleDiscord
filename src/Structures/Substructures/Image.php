b507d7f4f9deb441f036aead5cb4060aac88209c
Noah Overcash <smileytechguy@icloud.com>
2017-10-15 10:20:59 -0400

    Image substructure improvements/fixes

--- src/Structures/Substructures/Image.php
+++ src/Structures/Substructures/Image.php
@@ -2,22 +2,24 @@
 
 namespace SimpleDiscord\Structures\Substructures;
 
-abstract class Image {
+abstract class Image implements \SimpleDiscord\Structures\Substructures\Substructure {
 	private $url;
 
 	const BASE_URI = "https://cdn.discordapp.com/";
 
-	public function allowedFormats() : array;
-	public function getBaseUrl() : string;
+	public abstract function allowedFormats() : array;
+	public abstract function getBaseUrl() : string {
+		return self::BASE_URI.$this->url;
+	}
 
 	// will, by default, round up
-	// and use the webp format if specified is invalid
-	public function getURL(int $size=512, string $format="webp", bool $roundUp=true) : string {
+	// and use the png format if specified is invalid/nonexistent
+	public function getURL(int $size=512, string $format="png", bool $roundUp=true) : string {
 		$requestedPower = log($size, 2);
 		$requestedPower = $roundUp ? ceil($requestedPower) : floor($requestedPower);
 		$requestedPower = min(11, max(4, $requestedPower));
 
-		$requestedFormat = in_array($format, $this->allowedFormats()) ? $format : "webp";
+		$requestedFormat = in_array($format, $this->allowedFormats()) ? $format : "png";
 
 		return $this->getBaseUrl.".".$requestedFormat."?size=".pow(2, $requestedPower);
 	}
